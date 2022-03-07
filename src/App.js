319e5cac27871c394264edb0f185f5eb2cfe125f
carlos-osorio-developer <osorio.uis@gmail.com>
2022-03-07 17:35:04 -0500

    Set Routes using React-Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/header';
+import Rockets from './components/pages/rockets';
+import Missions from './components/pages/missions';
+import Profile from './components/pages/profile';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Rockets />} />
+        <Route path="/missions" element={<Missions />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </div>
   );
 }
