1cfdfe3e73d1965131719f0f27a45406777d4a0a
carlos-osorio-developer <osorio.uis@gmail.com>
2022-03-08 11:01:18 -0500

    Add mission component and structure for Missions page

--- src/components/mission.js
+++ src/components/mission.js
@@ -3,16 +3,17 @@ import PropTypes from 'prop-types';
 export default function Rocket(props) {
   const { name, description, status } = props;
   return (
-    <div>
-      <h2>{name}</h2>
-      <p>{description}</p>
-      <button type="button">{status}</button>
-    </div>
+    <tr>
+      <td>{name}</td>
+      <td>{description}</td>
+      <td>{status}</td>
+      <td><button type="button">Join mission</button></td>
+    </tr>
   );
 }
 
 Rocket.propTypes = {
   name: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
-  status: PropTypes.string.isRequired,
+  status: PropTypes.bool.isRequired,
 };
--- src/components/pages/missions.js
+++ src/components/pages/missions.js
@@ -1,6 +1,19 @@
+import Mission from '../mission';
+
 const Missions = () => (
   <div>
-    <h1>Missions</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Description</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <Mission name="Falcon 1" description="The Falcon 1 is a two-stage rocket designed and manufactured by SpaceX." status />
+      </tbody>
+    </table>
   </div>
 );
 
