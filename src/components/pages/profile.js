2fcb10a6fcbbdae973e3b68cfadbb8e0f1cd1d6e
carlos-osorio-developer <osorio.uis@gmail.com>
2022-03-09 18:39:04 -0500

    Add profile component with active missions/rockets

--- src/components/pages/profile.js
+++ src/components/pages/profile.js
@@ -1,7 +1,35 @@
-const Profile = () => (
-  <div>
-    <h1>Profile</h1>
-  </div>
-);
+import { useSelector } from 'react-redux';
+import style from './profile.module.scss';
+
+const Profile = () => {
+  const missions = useSelector((state) => state.missions.entities);
+  const rockets = useSelector((state) => state.rockets.rockets);
+
+  const activeMissions = missions.filter((mission) => mission.reserved);
+  const activeRockets = rockets.filter((rocket) => rocket.status);
+
+  return (
+    <div className={style.mainContainer}>
+      <section className={style.missionsContainer}>
+        <ul>
+          {activeMissions.map((mission) => (
+            <li key={mission.mission_id}>
+              {mission.mission_name}
+            </li>
+          ))}
+        </ul>
+      </section>
+      <section className={style.rocketsContainer}>
+        <ul>
+          {activeRockets.map((rocket) => (
+            <li key={rocket.id}>
+              {rocket.name}
+            </li>
+          ))}
+        </ul>
+      </section>
+    </div>
+  );
+};
 
 export default Profile;
